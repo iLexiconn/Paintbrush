b072a51ef93f7dff693b36ae38e8069ce54ff5b3
iLexiconn <ilexiconn@gmail.com>
2016-01-16 19:22:16 +0100

    Update build script

--- src/main/java/net/ilexiconn/paintbrush/Paintbrush.java
+++ src/main/java/net/ilexiconn/paintbrush/Paintbrush.java
@@ -11,11 +11,13 @@ import net.ilexiconn.paintbrush.server.ServerProxy;
 import net.ilexiconn.paintbrush.server.item.PaintbrushItem;
 import net.ilexiconn.paintbrush.server.message.MessageUpdatePaint;
 
-@Mod(modid = "paintbrush", name = "Paintbrush", version = "0.1.0")
+@Mod(modid = "paintbrush", name = "Paintbrush", version = Paintbrush.VERSION)
 public class Paintbrush {
     @SidedProxy(serverSide = "net.ilexiconn.paintbrush.server.ServerProxy", clientSide = "net.ilexiconn.paintbrush.client.ClientProxy")
     public static ServerProxy proxy;
     public static SimpleNetworkWrapper networkWrapper;
+
+    public static final String VERSION = "0.1.0";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/net/ilexiconn/paintbrush/client/ClientProxy.java
+++ src/main/java/net/ilexiconn/paintbrush/client/ClientProxy.java
@@ -3,7 +3,6 @@ package net.ilexiconn.paintbrush.client;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.ilexiconn.paintbrush.server.CommonEventHandler;
 import net.ilexiconn.paintbrush.server.ServerProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
