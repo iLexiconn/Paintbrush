4ecfd9bad298d0a4e803fbda871c50702eb4e9ea
iLexiconn <ilexiconn@gmail.com>
2016-02-08 15:46:34 +0100

    Break all the things

--- src/main/java/net/ilexiconn/paintbrush/client/render/PaintedBlockRenderer.java
+++ src/main/java/net/ilexiconn/paintbrush/client/render/PaintedBlockRenderer.java
@@ -1,40 +1,76 @@
 package net.ilexiconn.paintbrush.client.render;
 
 import net.ilexiconn.paintbrush.server.entity.PaintedBlockEntity;
-import net.ilexiconn.paintbrush.server.util.Paint;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.opengl.GL11;
+
+import java.util.Map;
+import java.util.WeakHashMap;
 
 @SideOnly(Side.CLIENT)
 public class PaintedBlockRenderer extends Render<PaintedBlockEntity> {
-    public PaintedBlockRenderer(RenderManager renderManagerIn) {
-        super(renderManagerIn);
+    public Minecraft mc;
+    public Map<PaintedBlockEntity, TextureData> textureDataMap = new WeakHashMap<>();
+
+    public PaintedBlockRenderer(RenderManager renderManager) {
+        super(renderManager);
+        mc = Minecraft.getMinecraft();
+    }
+
+    public TextureData getTextureData(PaintedBlockEntity entity) {
+        if (textureDataMap.containsKey(entity)) {
+            return textureDataMap.get(entity);
+        } else {
+            TextureData textureData = new TextureData(entity);
+            textureDataMap.put(entity, textureData);
+            return textureData;
+        }
     }
 
     @Override
     public void doRender(PaintedBlockEntity entity, double posX, double posY, double posZ, float yaw, float partialTicks) {
-        Minecraft mc = Minecraft.getMinecraft();
-
-        GlStateManager.disableCull();
+        TextureData textureData = getTextureData(entity);
+        /*GlStateManager.disableCull();
         GlStateManager.disableTexture2D();
         GlStateManager.disableAlpha();
         GlStateManager.disableBlend();
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glNormal3f(0.5F, 0.5F, 0.5F);
-        AxisAlignedBB bounds = entity.worldObj.getBlockState(entity.blockPos).getBlock().getSelectedBoundingBox(entity.worldObj, entity.blockPos);
+        AxisAlignedBB bounds = entity.worldObj.getBlockState(entity.blockPos).getBlock().getSelectedBoundingBox(entity.worldObj, entity.blockPos);*/
 
-        for (Paint paint : entity.paintList) {
+        for (EnumFacing facing : EnumFacing.VALUES) {
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(posX - 0.5F, posY, posZ - 0.5F);
+            Tessellator tessellator = Tessellator.getInstance();
+            WorldRenderer worldRenderer = tessellator.getWorldRenderer();
+            mc.getTextureManager().bindTexture(textureData.getPaintTextureLocation(facing));
+
+            worldRenderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+
+            switch (facing) {
+                case NORTH:
+                    worldRenderer.pos(0.0F, 0.0F, 1.0F - 0.001F).tex(0.0F, 0.0F).endVertex();
+                    worldRenderer.pos(0.0F, 0.0F, 1.0F - 0.001F).tex(1.0F, 0.0F).endVertex();
+                    worldRenderer.pos(0.0F, 0.0F, 1.0F - 0.001F).tex(1.0F, 1.0F).endVertex();
+                    worldRenderer.pos(0.0F, 0.0F, 1.0F - 0.001F).tex(0.0F, 1.0F).endVertex();
+                    System.out.println(facing);
+                    break;
+            }
+            
+            tessellator.draw();
+            GlStateManager.popMatrix();
+        }
+
+        /*for (Paint paint : entity.paintList) {
             GlStateManager.pushMatrix();
 
             int i = entity.getBrightnessForFace(paint.facing);
@@ -119,7 +155,7 @@ public class PaintedBlockRenderer extends Render<PaintedBlockEntity> {
         GlStateManager.enableCull();
         GlStateManager.enableTexture2D();
         GlStateManager.enableAlpha();
-        GlStateManager.enableBlend();
+        GlStateManager.enableBlend();*/
     }
 
     @Override
